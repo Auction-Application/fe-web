9d49dd1ec76b11c46a065982525e9509fe6ba905
shaishav-rgb <shaishavpaudel@gmail.com>
2026-06-10 07:46:37 +0545

    refactor: refactor styles and UI

--- apps/auction/src/app/user-entry/signup-page/verify-email/verify-email.ts
+++ apps/auction/src/app/user-entry/signup-page/verify-email/verify-email.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { DynamicValidatorMessage, ViewContainer } from '@auction/validators';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TuiInputPin } from '@taiga-ui/kit';
 
 @Component({
@@ -10,6 +12,9 @@ import { TuiInputPin } from '@taiga-ui/kit';
     ReactiveFormsModule,
     DynamicValidatorMessage,
     ViewContainer,
+    TuiButton,
+    TuiIcon,
+    RouterLink,
   ],
   templateUrl: './verify-email.html',
   styles: ``,
@@ -19,4 +24,12 @@ export class VerifyEmail {
     Validators.minLength(6),
     Validators.required,
   ]);
+
+  protected changeToSignup() {
+    console.log('Changed to Signup');
+  }
+
+  protected resendCode() {
+    console.log('Resend Code');
+  }
 }
