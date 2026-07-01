197ee8520f560ba532f059942918bab6f9854164
shaishav-rgb <shaishavpaudel@gmail.com>
2026-07-01 07:27:56 +0545

    feat: add item details form step

--- apps/auction/src/app/lot/configure-lot/configure-lot.component.ts
+++ apps/auction/src/app/lot/configure-lot/configure-lot.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { TuiStepper } from '@taiga-ui/kit';
+import { ItemDetailsComponent } from './steps/item-details/item-details.component';
 import { MediaUploadComponent } from './steps/media-upload/media-upload.component';
 
 @Component({
   selector: 'app-configure-lot',
-  imports: [TuiStepper, MediaUploadComponent],
+  imports: [TuiStepper, MediaUploadComponent, ItemDetailsComponent],
+  host: {
+    class: 'background',
+  },
   templateUrl: './configure-lot.component.html',
   styles: ``,
 })
--- apps/auction/src/app/lot/configure-lot/steps/item-details/item-details.component.ts
+++ apps/auction/src/app/lot/configure-lot/steps/item-details/item-details.component.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { DynamicValidatorMessage, ViewContainer } from '@auction/validators';
+import {
+  TuiButton,
+  TuiCell,
+  TuiIcon,
+  TuiInput,
+  TuiTitle,
+} from '@taiga-ui/core';
+import { TuiAvatar, TuiSelect, TuiTextarea } from '@taiga-ui/kit';
+import { TuiCard } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-item-details',
-  imports: [],
   templateUrl: './item-details.component.html',
-  styles: ``,
+  styleUrl: './item-details.component.scss',
+  host: {
+    class: 'background',
+  },
+  imports: [
+    ReactiveFormsModule,
+    DynamicValidatorMessage,
+    ViewContainer,
+    TuiInput,
+    TuiSelect,
+    TuiTextarea,
+    TuiButton,
+    TuiIcon,
+    TuiCell,
+    TuiTitle,
+    TuiAvatar,
+    TuiCard,
+  ],
 })
-export class ItemDetailsComponent {}
+export class ItemDetailsComponent {
+  @Output() readonly cancelled = new EventEmitter<void>();
+  @Output() readonly proceeded = new EventEmitter<void>();
+
+  protected readonly categories = [
+    'Paintings & Prints',
+    'Sculpture',
+    'Photography',
+    'Jewelry & Watches',
+    'Antiques & Collectibles',
+    'Wine & Spirits',
+    'Books & Manuscripts',
+    'Asian Art',
+  ];
+
+  protected readonly form = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    category: new FormControl<string | null>(null, [Validators.required]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(500),
+    ]),
+  });
+
+  protected get descriptionLength(): number {
+    return this.form.get('description')?.value?.length ?? 0;
+  }
+
+  protected onNext(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.proceeded.emit();
+  }
+}
